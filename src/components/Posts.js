f445edf53cbecf4456de787e69cb3b3e738f005c
Lucas <lucas@agtlucas.com>
2016-09-18 08:40:42 -0300

    Add Posts.js

---
+++ src/components/Posts.js
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+import { View, ListView, Text } from 'react-native'
+
+const Posts = ({ posts }) => {
+  return (
+    <View></View>
+  )
+}
+
+Posts.propTypes = {
+  posts: PropTypes.array.isRequired
+}
+
+export default Posts
